c96a19777f2751490a90f02e3fd48d768a02b8f0
Paranoj <e.behterev@yandex.ru>
2023-02-03 00:41:03 +0700

    8. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,47 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            var launch = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                var diff = System.currentTimeMillis() - launch;
+                if (diff < speed) {
+                    Thread.sleep(speed - diff);
+                }
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                launch = System.currentTimeMillis();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length > 2) {
+            throw new IllegalArgumentException("Not acceptable quantity of the parameters.");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
