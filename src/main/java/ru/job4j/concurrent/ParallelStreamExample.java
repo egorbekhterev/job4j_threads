b138acaa44fe6b58a9be9ca98592e6f1ab717b7b
Paranoj <e.behterev@yandex.ru>
2023-02-03 12:12:47 +0700

    10. Многопоточность в Stream API [#504930 #387785]

---
+++ src/main/java/ru/job4j/concurrent/ParallelStreamExample.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class ParallelStreamExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> stream = list.parallelStream();
+        System.out.println(stream.isParallel());
+        Optional<Integer> multiplication = list.stream()
+                .reduce((a, b) -> a * b);
+        System.out.println(multiplication.get());
+        stream.sequential();
+        System.out.println(stream.isParallel());
+        list.stream().parallel().peek(System.out::println).toList();
+        List<Integer> listOrdered = Arrays.asList(1, 4, 3, 5, 2);
+        listOrdered.stream().parallel().forEachOrdered(System.out::println);
+    }
+}
