af41647a4dbe1b912bd3dcccbab365208c11d446
Paranoj <e.behterev@yandex.ru>
2023-02-05 15:10:50 +0700

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/thread/visibility/ParseFile.java
@@ -0,0 +1,35 @@
+package ru.job4j.thread.visibility;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    private String content(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        int data;
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
+            while ((data = i.read()) > 0) {
+                 if (filter.test((char) data)) {
+                     output.append((char) data);
+                 }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public String defaultContent() {
+        return content(character -> character > 0);
+    }
+
+    public String contentWithoutUnicode() {
+        return content(character -> character < 0x80);
+    }
+}
