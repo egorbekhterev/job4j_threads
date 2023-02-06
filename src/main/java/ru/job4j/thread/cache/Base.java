e9864ddc326ea4862f1e85d20ebc0903ed9b53af
Paranoj <e.behterev@yandex.ru>
2023-02-07 00:58:50 +0700

    1. Неблокирующий кеш [#4741]

--- src/main/java/ru/job4j/thread/cache/Base.java
+++ src/main/java/ru/job4j/thread/cache/Base.java
@@ -30,8 +30,12 @@ public class Base {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Base base = (Base) o;
         return id == base.id && version == base.version;
     }
