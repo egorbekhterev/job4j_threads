77aeb18aac3e2ade7e7074e02553ee59a4856a2e
Paranoj <e.behterev@yandex.ru>
2023-02-07 00:57:19 +0700

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/thread/cache/Cache.java
@@ -0,0 +1,36 @@
+package ru.job4j.thread.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    @Override
+    public String toString() {
+        return "Cache{" +
+                "memory=" + memory +
+                '}';
+    }
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        return memory.computeIfPresent(model.getId(), (integer, stored) -> {
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal.");
+            }
+            return new Base(model.getId(), model.getVersion() + 1);
+        }) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+
+    public Base getValue(int id) {
+        return memory.get(id);
+    }
+}
---
+++ src/test/java/ru/job4j/thread/cache/CacheTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.thread.cache;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CacheTest {
+    private final Cache cache = new Cache();
+
+    @Test
+    void whenAddSuccessfully() {
+        Base base = new Base(1, 0);
+        assertThat(cache.add(base)).isTrue();
+        base.setName("base");
+        assertThat(base.getName()).isEqualTo("base");
+    }
+
+    @Test
+    void whenAddBaseWithTheSameId() {
+        Base base = new Base(1, 0);
+        Base base1 = new Base(1, 0);
+        cache.add(base);
+        assertThat(cache.add(base1)).isFalse();
+    }
+
+    @Test
+    void whenDeleteSuccessfully() {
+        Base base = new Base(1, 0);
+        cache.add(base);
+        cache.delete(base);
+        assertThat(cache.getValue(base.getId())).isNull();
+    }
+
+    @Test
+    void whenUpdateSuccessfully() {
+        Base base = new Base(1, 0);
+        cache.add(base);
+        assertThat(cache.update(base)).isTrue();
+        assertThat(cache.getValue(base.getId()).getId()).isEqualTo(1);
+    }
+
+    @Test
+    void whenUpdateAlreadyChangedVersion() {
+        Base base = new Base(1, 0);
+        cache.add(base);
+
+        Base user1 = cache.getValue(1);
+        user1.setName("User 1");
+
+        Base user2 = cache.getValue(1);
+        user2.setName("User 2");
+
+        assertThatThrownBy(() -> {
+            cache.update(user1);
+            cache.update(user2);
+        }).hasMessageMatching("Versions are not equal.");
+    }
+}
